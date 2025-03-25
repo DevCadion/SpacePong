ddca4ba4902829ca7a4a153fb41d26b1801f8692
DevCadion <caio.santos.m@hotmail.com>
2025-03-25 15:06:04 -0300

    pongs

--- scripts/ball.gd
+++ scripts/ball.gd
@@ -5,6 +5,7 @@ var started = false
 var start_speed = 500
 var incremental_speed = 1.02
 var angle = [-250, 250]
+var pongs = 0
 
 func _physics_process(delta):
 	if Input.is_action_pressed("vai") and started == false:
@@ -12,7 +13,13 @@ func _physics_process(delta):
 	if started:
 		var collisions = move_and_collide(velocity * delta)
 		if collisions != null:
-			velocity = velocity.bounce(collisions.get_normal()) * incremental_speed
+			if collisions.get_collider().name == "TopWall":
+				pongs += 1
+				velocity = velocity.bounce(collisions.get_normal()) * incremental_speed
+				
+			else:
+				velocity = velocity.bounce(collisions.get_normal())
+		print(pongs)
 
 func start_game():
 	started = true
---
+++ scripts/game.gd
@@ -0,0 +1,8 @@
+extends Node2D
+
+@onready var  ball= $Ball
+
+@onready var label_pongs = $Pongs
+
+func _process(delta): 
+	label_pongs.text = str(ball.pongs)
